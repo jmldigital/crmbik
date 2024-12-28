078893a9dc836e890e73228467ec3f3dbd64acc6
jml.digital <jml.digital>
2024-12-28 06:46:48 +0300

    Update vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  base: '/crmbik/', // Правильно - соответствует имени вашего репозитория
 })
