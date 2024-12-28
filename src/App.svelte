2b26e14756a3f16012572725749072547700ff03
jml.digital <jml.digital>
2024-12-28 07:15:52 +0300

    Update vite config for GitHub Pages

--- src/App.svelte
+++ src/App.svelte
@@ -7,15 +7,19 @@
 
   console.log('App.svelte: Component initialized');
   
+  // Для GitHub Pages
   let url = window.location.pathname;
+  const base = "/crmbik";
+  
   console.log('App.svelte: Current URL:', url);
+  console.log('App.svelte: Base path:', base);
 </script>
 
-<Router {url}>
+<Router {url} {base}>
   <main>
-    <Route path="/" component={Login} />
-    <Route path="/clients" component={Clients} />
-    <Route path="/register" component={Register} />
-    <Route path="/login" component={Login} />
+    <Route path="" component={Login} /> <!-- Изменено с "/" на "" -->
+    <Route path="clients" component={Clients} /> <!-- Убран начальный слеш -->
+    <Route path="register" component={Register} /> <!-- Убран начальный слеш -->
+    <Route path="login" component={Login} /> <!-- Убран начальный слеш -->
   </main>
 </Router>
